e720ee9a3dd5e004e3cee84b343394054370394d
mnajda <mateusznajda@gmail.com>
2017-11-08 07:18:20 +0100

    Add additional menu entry

--- tsp_project1/main.cpp
+++ tsp_project1/main.cpp
@@ -9,10 +9,11 @@ int main(int argc, char** argv)
     std::string filename;
     do
     {
-        std::cout << "1. Test using matrix." << std::endl;
+        std::cout << "1. Test using full matrix." << std::endl;
         std::cout << "2. Test using EUC_2D list." << std::endl;
-        std::cout << "3. Test using matrix and bruteforce for comparison." << std::endl;
-        std::cout << "4. Test (time info only)" << std::endl;
+        std::cout << "3. Test using Lower Diagonal Row matrix." << std::endl;
+        std::cout << "4. Test using full matrix and bruteforce for comparison." << std::endl;
+        std::cout << "5. Test (time info only)" << std::endl;
         std::cout << "0. Exit" << std::endl;
         std::cout << std::endl;
         std::cin >> choice;
@@ -64,6 +65,25 @@ int main(int argc, char** argv)
                 std::cin >> filename;
 
                 Parser parser;
+                parser.loadLowerDiagonalRow(filename);
+                Instance instance(parser.getCitiesList());
+                instance.printSolution();
+                instance.printTime();
+            }
+            catch (const std::runtime_error& ex)
+            {
+                std::cout << ex.what() << std::endl;
+            }
+            break;
+        }
+        case '4':
+        {
+            try
+            {
+                std::cout << "Filename: \n";
+                std::cin >> filename;
+
+                Parser parser;
                 parser.loadCitiesMatrix(filename);
                 Instance instance(parser.getcitiesMatrix(), 1);
             }
@@ -73,7 +93,7 @@ int main(int argc, char** argv)
             }
             break;
         }
-        case '4':
+        case '5':
         {
             try
             {
